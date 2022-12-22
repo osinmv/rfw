1e07202482b8e42cffe56b9e2ff552e89889ff94
osinmv <31288523+osinmv@users.noreply.github.com>
2022-12-22 02:17:37 -0500

    Added validation and part of argument formation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use exitcode;
 use std::env;
-use std::error::Error;
-use std::process::Command;
-
+use std::net::Ipv4Addr;
+use std::process::{self, Command};
+use std::str::FromStr;
 fn run_iptables(arguments: &String) -> (Vec<u8>, i32) {
     let output = Command::new("iptables")
         .arg(arguments)
@@ -18,13 +19,39 @@ fn run_iptables(arguments: &String) -> (Vec<u8>, i32) {
 }
 
 fn validate_arguments(args: &Vec<String>) {
-    
+    if args.len() != 3 {
+        eprintln!("Incorrect number of arguments");
+        process::exit(exitcode::DATAERR);
+    }
+    if args[1] != "unban" && args[1] != "ban" {
+        eprintln!("Unknown command {}", args[1]);
+        process::exit(exitcode::DATAERR);
+    }
+    match Ipv4Addr::from_str(&args[2]) {
+        // on success do nothing
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(exitcode::DATAERR);
+        }
+    };
+}
+fn form_arguments(args: &Vec<String>) -> String {
+    let ip_address = &args[2];
+    let argumets_string: String;
+    if args[1] == "unban" {
+        argumets_string = "-L".to_string();
+    } else {
+        // dropping all conections from given IP address
+        argumets_string = format!("-A INPUT -s {} -j DROP", ip_address);
+    }
+    return argumets_string;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1]);
-    let argumets = "-L".to_string();
+    validate_arguments(&args);
+    let argumets = form_arguments(&args);
     let cmd_result = run_iptables(&argumets);
     let out_string = String::from_utf8(cmd_result.0).expect("Could not read iptable output");
     println!("{out_string}");
